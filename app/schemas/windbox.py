a504102f5ee328b005dc8ec8e35d0d10f7706ecb
Fabian Volkers <fabian.volkers@gmail.com>
2021-10-19 20:31:55 +0200

    Create windbox pydantic schema

---
+++ app/schemas/windbox.py
@@ -0,0 +1,15 @@
+from typing import List, Optional
+
+from pydantic import BaseModel
+
+class WindboxBase(BaseModel):
+    hostname: str
+
+class WindboxCreate(WindboxBase):
+    pass
+
+class Windbox(WindboxBase):
+    id: int
+
+    class Config:
+        orm_mode = True
