5c2d1c8b07ff3052dd3f71f42caec1473bb32137
Fabian Volkers <fabian.volkers@gmail.com>
2021-10-22 00:27:14 +0200

    Add comments to windbox schema

--- app/schemas/schemas_windbox.py
+++ app/schemas/schemas_windbox.py
@@ -2,15 +2,19 @@ from typing import List, Optional
 
 from pydantic import BaseModel
 
+# Shared properties
 class WindboxBase(BaseModel):
     hostname: str
 
+# Properties to receive on item creation
 class WindboxCreate(WindboxBase):
     pass
 
+# Properties to receive on item update
 class WindboxUpdate(WindboxBase):
     pass
 
+# Properties shared by models stored in DB
 class Windbox(WindboxBase):
     id: int
 
