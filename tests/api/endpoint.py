1665d41749f49ef59450aa7c7ead3c154a3b5750
Fabian Volkers <fabian.volkers@gmail.com>
2021-11-05 15:30:31 +0100

    Add generic BaseTestEndpoint

---
+++ tests/api/endpoint.py
@@ -0,0 +1,67 @@
+
+
+from typing import Any, Dict, Generator
+from _pytest.monkeypatch import resolve
+from fastapi.testclient import TestClient
+import pytest
+from requests.models import Response
+from app.dependencies import get_settings
+from app.main import app as fa_app
+
+
+@pytest.mark.usefixtures("get_settings_override")
+class BaseTestEndpoint():
+
+    create_obj_gen: Generator
+    create_obj_cls: Any
+    data: Dict
+    endpoint: str
+
+    
+    @pytest.fixture
+    def client(self, get_settings_override):
+        fa_app.dependency_overrides[get_settings] = lambda: get_settings_override
+        yield TestClient(fa_app)
+
+    @pytest.fixture
+    def create_obj(self):
+        pass
+
+    def test__list(self, client, create_obj):
+        response = client.get(self.endpoint)
+
+        assert response.status_code == 200
+
+        data = response.json()
+        assert len(data) == 1
+
+        for key in self.create_obj_cls.__table__.columns.keys():
+            assert data[0][key] == getattr(create_obj, key)
+
+    def test__read(self, client, create_obj):
+        response = client.get(f"{self.endpoint}/{create_obj.id}")
+        assert response.status_code == 200
+
+        data = response.json()
+        for key in self.create_obj_cls.__table__.columns.keys():
+            assert data[key] == getattr(create_obj, key)
+
+    def test__create(self, client):
+        response = client.post(f"{self.endpoint}/", json=self.data)
+
+        assert response.status_code == 200
+        data = response.json()
+
+        for key in self.data.keys():
+            assert data[key] == self.data[key]
+
+        assert "id" in data
+        obj_id = data["id"]
+
+        response = client.get(f"{self.endpoint}/{obj_id}")
+        assert response.status_code == 200, response.text
+        data = response.json()
+        for key in self.data.keys():
+            assert data[key] == self.data[key]
+
+        assert data["id"] == obj_id
