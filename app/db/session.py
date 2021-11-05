a18443e21aedb5c3c9e9a3528922618c97f7a81f
Fabian Volkers <fabian.volkers@gmail.com>
2021-11-05 11:19:38 +0100

    Add get_db_session function

---
+++ app/db/session.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.engine.base import Engine
+from sqlalchemy.orm import sessionmaker
+
+
+def get_db_session(database_uri: str) -> sessionmaker:
+
+    engine: Engine = create_engine(
+        database_uri,
+        pool_pre_ping=True,
+        connect_args={"check_same_thread": False}
+        )
+
+    return sessionmaker(autocommit=False, autoflush=False, bind=engine)
