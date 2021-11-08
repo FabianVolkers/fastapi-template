422f549df516f9f4ed50d0c38252e4f5949f63f9
Fabian Volkers <fabian.volkers@gmail.com>
2021-11-08 11:57:42 +0100

    Refactor to use override_get_settings_fixture

--- tests/utils/test_feature_flags.py
+++ tests/utils/test_feature_flags.py
@@ -28,21 +28,12 @@ def test__get_return_value(test_input, expected):
     assert return_type == expected
 
 
-@pytest.fixture
-def override_get_feature_flag_settings():
-    class _TestFeatureFlagSettings(_TestSettings):
-        flag_is_enabled: bool = True
-        flag_is_disabled: bool = False
-    settings = _TestFeatureFlagSettings()
-    return settings
-
-
 @pytest.mark.parametrize("test_input,expected", [
     ('flag_is_disabled', False),
     ('flag_does_not_exist', False),
     ("flag_is_enabled", True)
 ])
-def test__get_flag_status(test_input, expected, override_get_feature_flag_settings):
-    flag_enabled = get_flag_status(test_input, override_get_feature_flag_settings)
+def test__get_flag_status(test_input, expected, override_get_settings_fixture):
+    flag_enabled = get_flag_status(test_input, override_get_settings_fixture)
 
     assert flag_enabled == expected
