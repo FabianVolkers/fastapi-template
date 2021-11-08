607eca76500ca5eb22591ea3602fdeb70be5c0f4
Fabian Volkers <fabian.volkers@gmail.com>
2021-11-08 02:09:27 +0100

    Refactor to get_return_values()

--- app/utils/feature_flags.py
+++ app/utils/feature_flags.py
@@ -26,13 +26,7 @@ def feature_flag(
             settings = get_settings()
             settings_var = f"flag_{flag_name}"
 
-            override_return_val = disabled_return_val == "no_override_passed"
-
-            if override_return_val:
-                try:
-                    return_val = get_type_hints(func)['return']
-                except KeyError:
-                    return_val = None
+            return_val = get_return_value(func, disabled_return_val)
 
             try:
                 flag_enabled = settings.dict()[settings_var]
@@ -52,3 +46,17 @@ def feature_flag(
         return wrapper
 
     return feature_flag_decorator
+
+
+def get_return_value(func, override_return_value):
+    if override_return_value:
+
+        type_hints  = get_type_hints(func)
+
+        if 'return' in type_hints:
+            return type_hints['return']
+        else:
+            return None
+
+def get_flag_status(flag_name):
+    
