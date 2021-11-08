12c7744f35ab824da398cb825e543198f40a74e3
Fabian Volkers <fabian.volkers@gmail.com>
2021-11-08 17:18:46 +0100

    Add tests for feature_flag decorator

--- tests/utils/test_feature_flags.py
+++ tests/utils/test_feature_flags.py
@@ -7,6 +7,7 @@ from app.utils.feature_flags import (
 
 from tests.conftest import override_get_settings
 
+
 def func_a(a):
     return a
 
@@ -45,3 +46,48 @@ def test__get_flag_status(test_input, expected):
         get_settings.assert_called_once()
 
         assert flag_enabled == expected
+
+
+@feature_flag("is_enabled")
+def enabled_feature():
+    return "This feature is enabled"
+
+
+@feature_flag("is_disabled")
+def disabled_feature():
+    return "This feature is disabled"
+
+
+@feature_flag("does_not_exist")
+def undefined_feature():
+    return "This feature flag is not defined"
+
+
+@feature_flag("is_disabled")
+def disabled_feature_typed() -> str:
+    return "This feature is disabled and typed"
+
+
+@feature_flag("is_disabled", int)
+def disabled_feature_typed_override() -> str:
+    return "egal"
+
+
+@pytest.mark.parametrize("test_input,expected", [
+    ("enabled_feature", "This feature is enabled"),
+    ("disabled_feature", None),
+    ("undefined_feature", None),
+    ("disabled_feature_typed", str),
+    ("disabled_feature_typed_override", int)
+])
+def test__feature_flag_decorator(test_input, expected):
+    with mock.patch('app.dependencies.get_settings') as settings_mock:
+        settings_mock.return_value = override_get_settings()
+
+        from app.dependencies import get_settings
+
+        return_value = eval(test_input)()
+
+        get_settings.assert_called_once()
+        
+        assert return_value == expected
