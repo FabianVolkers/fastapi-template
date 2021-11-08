982f5baa1535044da41e8490fecb569b4cad0a24
Fabian Volkers <fabian.volkers@gmail.com>
2021-11-08 02:15:02 +0100

    Use get_wireguard_router() to enable feature flag

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ def create_app():
 
     app = FastAPI()
 
-    app.include_router(wireguard.router)
+    app.include_router(wireguard.get_wireguard_router())
     app.include_router(windbox.router)
 
     @app.get("/")
