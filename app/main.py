200d8c47a41510e02c9387ab25b2f39cd7be1d38
Fabian Volkers <fabian.volkers@gmail.com>
2021-11-11 17:10:19 +0100

    Add docstrings

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 def create_app():
+    """Create FastAPI app instance"""
     from fastapi import FastAPI
 
     from app.routers import windbox, wireguard
@@ -17,14 +18,22 @@ def create_app():
 
 app = create_app()
 
+"""
+Functions for poetry scripts
+Run with poetry run <script_name>
+Defined scripts can be found in `pyproject.toml`
+"""
+
 
 def run():
+    """ Runs the FastAPI app for development using uvicorn"""
     import uvicorn
 
     uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
 
 
 def setup_db():
+    """Setup or update the database using alembic"""
     import os
 
     from alembic import command
@@ -35,21 +44,25 @@ def setup_db():
 
 
 def test():
+    """Run tests using pytest"""
     import subprocess
     subprocess.call(["pytest", "tests/"])
 
 
 def lint():
+    """Lint code using flake8"""
     import subprocess
     subprocess.call(["flake8", "app", "tests"])
 
 
 def isort():
+    """Sort imports using isort"""
     import subprocess
     subprocess.call(["isort", "app", "tests"])
 
 
 def autopep8():
+    """Format code using autopep8"""
     import subprocess
     subprocess.call([
         "autopep8",
@@ -63,16 +76,19 @@ def autopep8():
 
 
 def format():
+    """Format code using both isort and autopep8"""
     isort()
     autopep8()
 
 
 def check_isort():
+    """Check if imports are sorted correctly"""
     import subprocess
     subprocess.call(["isort", "app", "tests", "--check"])
 
 
 def ci():
+    """Run style guide checks and tests combined for CI"""
     check_isort()
     lint()
     test()
